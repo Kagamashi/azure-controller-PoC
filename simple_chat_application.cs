5e15bd1d2aac852b2e5db353a93399b57ee7cd60
Mikołaj <mikolaj.borecki1@gmail.com>
2022-03-29 23:33:52 +0200

    Update simple_chat_application.cs

--- simple_chat_application.cs
+++ simple_chat_application.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
+namespace RabbitMQChat
+{
+    class Program
     {
         static void Main(string[] args)
         {
